c7edd90dfb12f53fcf85b4921fdf3f6be3ce7188
Manuella Sampaio <manuella.sampaio@estudante.ifms.edu.br>
2026-06-25 07:31:31 -0400

    ajuste database.js

--- database/database.js
+++ database/database.js
@@ -10,89 +10,59 @@ export async function getDB() {
 }
 
 export async function createTable() {
-  const database = await getDB();
+  try {
+    const database = await getDB();
 
-  await database.execAsync(`
-    CREATE TABLE IF NOT EXISTS tarefas (
-      id INTEGER PRIMARY KEY AUTOINCREMENT,
-      titulo TEXT,
-      descricao TEXT,
-      status INTEGER,
-      foto TEXT
-    );
-  `);
-
+    await database.execAsync(`
+      CREATE TABLE IF NOT EXISTS tarefas (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        titulo TEXT,
+        descricao TEXT,
+        status INTEGER,
+        foto TEXT
+      );
+    `);
+  } catch(error) {
+    console.log(error.message);
 }
 
 export async function insertTask(titulo, descricao) {
-
   const database = await getDB();
-
   await database.runAsync(
-    `
-      INSERT INTO tarefas
-      (titulo, descricao, status, foto)
-      VALUES (?, ?, ?, ?)
-    `,
-    [
-      titulo,
-      descricao,
-      0,
-      ''
-    ]
+    `INSERT INTO tarefas (titulo, descricao, status, foto) VALUES (?, ?, ?, ?)`,
+    [titulo, descricao, 0, '']
   );
-
 }
 
 export async function getTasks() {
-
   const database = await getDB();
 
-  const result =
-    await database.getAllAsync(
-      `
-        SELECT *
-        FROM tarefas
-      `
+  const result = await database.getAllAsync(
+      `SELECT * FROM tarefas`
     );
 
   return result;
-
 }
 
 export async function deleteTask(id) {
-
   const database = await getDB();
 
   await database.runAsync(
-    `
-      DELETE FROM tarefas
-      WHERE id = ?
-    `,
+    `DELETE FROM tarefas
+     WHERE id = ?`,
     [id]
   );
-
 }
 
-export async function completeTask(
-  id,
-  foto
-) {
-
+export async function completeTask(id, foto) {
   const database = await getDB();
 
   await database.runAsync(
-    `
-      UPDATE tarefas
-      SET
+    `UPDATE tarefas
+     SET
         status = 1,
         foto = ?
-      WHERE id = ?
-    `,
-    [
-      foto,
-      id
-    ]
+     WHERE id = ?`,
+    [foto, id]
   );
-
 }
