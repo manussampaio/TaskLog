bff2602dc60371728d85abfa79cd71da7beb0b73
Manuella Sampaio <manuella.sampaio@estudante.ifms.edu.br>
2026-06-25 08:45:11 -0400

    commit geral

--- components/TaskComplete.js
+++ components/TaskComplete.js
@@ -10,64 +10,43 @@ import { MaterialIcons }
 from '@expo/vector-icons';
 
 export default function TaskComplete(props) {
-
   return (
-
     <View style={styles.container}>
-
       <View style={styles.check}>
-        <Text style={styles.checkText}>
+        <Text style={styles.checkIcone}>
           ✓
         </Text>
       </View>
-
       <View style={styles.card}>
-
         <Text style={styles.titulo}>
           {props.task.task}
         </Text>
-
         <Text style={styles.descricao}>
           {props.task.descricao}
         </Text>
-
         {props.task.foto ? (
             <Image
-                source={{
-                    uri: props.task.foto
-                }}
+                source={{uri: props.task.foto}}
                 style={styles.foto}
             />
         ) : null}
-
-        <TouchableOpacity
-  onPress={props.deleteTask}
-  style={styles.delete}
->
-
-  <MaterialIcons
-    name="delete"
-    size={28}
-    color="#EF4444"
-  />
-
-</TouchableOpacity>
-
+        <TouchableOpacity onPress={props.deleteTask} style={styles.delete}>
+          <MaterialIcons
+            name="delete"
+            size={28}
+            color="#EF4444"
+            />
+        </TouchableOpacity>
       </View>
-
     </View>
-
   );
-
 }
 
 const styles = StyleSheet.create({
-
   container: {
     flexDirection: 'row',
     margin: 10,
   },
-
   check: {
     width: 50,
     height: 50,
@@ -77,39 +56,32 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginRight: 10,
   },
-
-  checkText: {
+  checkIcone: {
     color: '#fff',
     fontSize: 18,
     fontWeight: 'bold',
   },
-
   card: {
     flex: 1,
     backgroundColor: '#1E293B',
     borderRadius: 10,
     padding: 10,
   },
-
   titulo: {
     color: '#fff',
     fontWeight: 'bold',
   },
-
   descricao: {
     color: '#CBD5E1',
   },
-
   foto: {
   width: '100%',
   height: 200,
   borderRadius: 10,
   marginTop: 10,
 },
-
 delete: {
   alignSelf: 'flex-end',
   marginTop: 10,
 },
-
 });
